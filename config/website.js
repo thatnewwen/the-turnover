86d910e21ab0accd9a263638411cbefe38dff342
alexwen.creates@gmail.com <alexwen.creates@gmail.com>
2021-01-05 03:10:18 -0600

    Build.

--- config/website.js
+++ config/website.js
@@ -6,11 +6,11 @@ module.exports = {
   headline: "The Turnover", // Headline for schema.org JSONLD
   url: "https://theturnover.club", // Domain of your site. No trailing slash!
   siteLanguage: "en", // Language Tag on <html> element
-  logo: "/logos/logo-1024.png", // Used for SEO
+  logo: "static/logos/logo-1024.png", // Used for SEO
   ogLanguage: "en_US", // Facebook Language
 
   // JSONLD / Manifest
-  favicon: "src/favicon.png", // Used for manifest favicon generation
+  favicon: "static/logos/logo-1024.png", // Used for manifest favicon generation
   shortName: "Prismic", // shortname for manifest. MUST be shorter than 12 characters
   author: "commonworks", // Author for schemaORGJSONLD
   themeColor: "#3D63AE",
--- src/templates/post.jsx
+++ src/templates/post.jsx
@@ -113,40 +113,6 @@ export const pageQuery = graphql`
               }
             }
           }
-          ... on PrismicPostBodyCodeBlock {
-            slice_type
-            id
-            primary {
-              code_block {
-                html
-              }
-            }
-          }
-          ... on PrismicPostBodyQuote {
-            slice_type
-            id
-            primary {
-              quote {
-                html
-                text
-              }
-            }
-          }
-          ... on PrismicPostBodyImage {
-            slice_type
-            id
-            primary {
-              image {
-                localFile {
-                  childImageSharp {
-                    fluid(maxWidth: 1200, quality: 90) {
-                      ...GatsbyImageSharpFluid_withWebp
-                    }
-                  }
-                }
-              }
-            }
-          }
         }
       }
     }
